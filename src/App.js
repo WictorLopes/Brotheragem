b5f8ff1abf0300a297e26beaafd4cbfaec6a455f
Wictor <wictorlopes258@gmail.com>
2022-09-29 17:18:38 -0300

    inicio da página home

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import "./App.css";
+import React from "react";
+import { ImageBackground, View } from "react-native";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+
 function App() {
   return (
     <div className="App">
-      <h1>PROJETO BROTHERAGEM</h1>
-      <p>Aqui contaremos uma história de 4 amigos</p>
+      <header className="header">
+        <h1>Brotheragem</h1>
+        <h1>Quem somos</h1>
+        <h1>História</h1>
+      </header>
+      <body className="body">
+        <p>Aqui contaremos uma história de 4 amigos</p>
+        <View>
+          <ImageBackground
+            source={require("./imgs/foto1.png")}
+            style={{ width: 400, height: 400 }}
+          ></ImageBackground>
+        </View>
+      </body>
     </div>
   );
 }
---
+++ src/Carousel.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => (
+  <Carousel autoPlay>
+    <div>
+      <img alt=""  src="./imgs/foto1.png" />
+
+      <img alt="" src="./imgs/foto2.png" />
+    </div>
+  </Carousel>
+);
