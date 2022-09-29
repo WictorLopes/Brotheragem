015fa845083e3a0a5ad6de18ca975ced8b250702
Wictor <wictorlopes258@gmail.com>
2022-09-29 16:20:41 -0300

    incrementando o paragrafo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <h1>PROJETO BROTHERAGEM</h1>
-      <p>Aqui contaremos uma história</p>
+      <p>Aqui contaremos uma história de 4 amigos</p>
     </div>
   );
 }
