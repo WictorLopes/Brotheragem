b87de88a27ed106070a1eb192628b379071e3eb9
Gabriel Marcio <gabrielmarcio13@hotmail.com>
2022-09-29 16:24:17 -0300

    Adicionando novo parágrafo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
     <div className="App">
       <h1>PROJETO BROTHERAGEM</h1>
       <p>Aqui contaremos uma história de 4 amigos</p>
+      <h2>Novo parágrafo</h2>
     </div>
   );
 }
