4f03066ec167d82789dc2904c27c40c90a8dd2ea
Wictor <wictorlopes258@gmail.com>
2022-09-29 21:45:51 -0300

    adição do logo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 import React from "react";
-// import { ImageBackground, View } from "react-native";
+import "bootstrap/dist/css/bootstrap.css";
 import { CarouselFunction } from "./Carousel";
 
 function App() {
   return (
     <div className="App">
       <header className="header">
-        <h1>Brotheragem</h1>
-        <h1>Quem somos</h1>
-        <h1>História</h1>
+        <img alt="" src={require("./img/logo.png")} />
+        <div className="divLinksHeader">
+          <h1>Quem somos</h1>
+          <h1 style={{paddingLeft: 200}}>História</h1>
+        </div>
       </header>
       <body className="body">
         <p>Aqui contaremos uma história de 4 amigos</p>
